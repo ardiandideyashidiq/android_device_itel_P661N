d8f734da700980ffc7a2df4ce3a5cf1ead601138
Archellion <estrogajohnrhys@gmail.com>
2026-05-23 20:36:20 +0000

    use gms

--- lineage_P661N.mk
+++ lineage_P661N.mk
@@ -35,7 +35,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
 
 WITH_GAPPS := true
-WITH_GMS := false
+WITH_GMS := true
 
 # Ship Basic Call Recorder App
 WITH_BCR := true
